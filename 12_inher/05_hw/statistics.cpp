80b17413e7e5c1c298bec8a4037f2a6fb77c5983
ivales <i.leskin@mail.ru>
2025-05-23 08:59:04 +0300

    statistics.cpp: add division by zero checking

--- 12_inher/05_hw/statistics.cpp
+++ 12_inher/05_hw/statistics.cpp
@@ -71,6 +71,7 @@ public:
 	}
 
 	double eval() const override {
+		if (m_count == 0) throw std::domain_error("Division by zero!");
 		return m_mean/m_count;
 	}
 
@@ -97,6 +98,7 @@ public:
 
 	double eval() const override {
 		double disp = 0;
+		if (m_count == 0) throw std::domain_error("Division by zero!");
 		double mean = m_mean/m_count;
 		for (double number:m_numbers) {disp += pow(number - mean, 2);}
 		return sqrt(disp/m_count-1);
