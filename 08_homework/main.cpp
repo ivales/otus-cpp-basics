c4dd53dcc2aeed1976acd4fe11d6e0b8f6ea5b2c
ivales <i.leskin@mail.ru>
2025-08-24 19:40:33 +0300

    main.cpp: try to insert multithreadung inside of hack

--- 08_homework/main.cpp
+++ 08_homework/main.cpp
@@ -16,34 +16,9 @@ void replaceLastFourBytes(std::vector<char> &data, uint32_t value) {
 
 std::atomic<bool> isRunning(true);
 
-/**
- * @brief Формирует новый вектор с тем же CRC32, добавляя в конец оригинального
- * строку injection и дополнительные 4 байта
- * @details При формировании нового вектора последние 4 байта не несут полезной
- * нагрузки и подбираются таким образом, чтобы CRC32 нового и оригинального
- * вектора совпадали
- * @param original оригинальный вектор
- * @param injection произвольная строка, которая будет добавлена после данных
- * оригинального вектора
- * @return новый вектор
- */
-void hack(const std::vector<char> &original,
-                       const std::string &injection, size_t start, size_t end, std::vector<char> &badData) {
-                        
-  const uint32_t originalCrc32 = crc32(original.data(), original.size());
-  std::vector<char> result(original.size() + injection.size() + 4);
-  auto it = std::copy(original.begin(), original.end(), result.begin());
-  std::copy(injection.begin(), injection.end(), it);
-
-    /*
-    * Внимание: код ниже крайне не оптимален.
-    * В качестве доп. задания устраните избыточные вычисления
-    */
-
-  auto resultCrc32 = crc32(result.data() + original.size(), injection.size(), ~originalCrc32);
-
-  
-  for (size_t i = start; i < end; ++i) {
+void multi_hack(size_t start, size_t end, const std::vector<char> &original, const std::string &injection,
+   std::vector<char> result, uint32_t &originalCrc32, uint32_t &resultCrc32, std::vector<char> &badData) {
+    for (size_t i = start; i < end; ++i) {
     if (isRunning) {
     // Заменяем последние четыре байта на значение i
       replaceLastFourBytes(result, uint32_t(i));
@@ -56,15 +31,53 @@ void hack(const std::vector<char> &original,
         isRunning = false;
         return;
       }
-      // Отображаем прогресс
-      // if (i % 1000 == 0) {
-      //   std::cout << "progress: "
-      //             << static_cast<double>(i) / static_cast<double>(end)
-      //             << std::endl;
-      // }
     }
     else return;
   }
+}
+
+/**
+ * @brief Формирует новый вектор с тем же CRC32, добавляя в конец оригинального
+ * строку injection и дополнительные 4 байта
+ * @details При формировании нового вектора последние 4 байта не несут полезной
+ * нагрузки и подбираются таким образом, чтобы CRC32 нового и оригинального
+ * вектора совпадали
+ * @param original оригинальный вектор
+ * @param injection произвольная строка, которая будет добавлена после данных
+ * оригинального вектора
+ * @return новый вектор
+ */
+void hack(const std::vector<char> &original,
+                       const std::string &injection, std::vector<char> &badData) {
+                        
+  const uint32_t originalCrc32 = crc32(original.data(), original.size());
+  std::vector<char> result(original.size() + injection.size() + 4);
+  auto it = std::copy(original.begin(), original.end(), result.begin());
+  std::copy(injection.begin(), injection.end(), it);
+
+    /*
+    * Внимание: код ниже крайне не оптимален.
+    * В качестве доп. задания устраните избыточные вычисления
+    */
+
+  auto resultCrc32 = crc32(result.data() + original.size(), injection.size(), ~originalCrc32);
+  
+      const size_t maxVal = std::numeric_limits<uint32_t>::max();
+    // size_t threadsNumber = std::thread::hardware_concurrency();
+    size_t threadsNumber = 4;
+    std::vector<std::thread> threads;
+
+    size_t chunk_size = maxVal / threadsNumber;
+    for (size_t i = 0; i < threadsNumber; ++i) {
+        size_t start = i * chunk_size;
+        size_t end = (i == threadsNumber - 1) ? maxVal : (i + 1) * chunk_size;
+        threads.emplace_back(multi_hack, start, end, std::cref(original), std::cref(injection), result,
+         std::ref(originalCrc32), std::ref(resultCrc32), std::ref(badData));
+    }
+
+        for (auto& thread : threads) {
+        thread.join();
+    }
 }
 
 int main(int argc, char **argv) {
@@ -79,21 +92,8 @@ int main(int argc, char **argv) {
   try {
     const std::vector<char> data = readFromFile(argv[1]);
     std::vector<char> badData;
-    const size_t maxVal = std::numeric_limits<uint32_t>::max();
-    // size_t threadsNumber = std::thread::hardware_concurrency();
-    size_t threadsNumber = 4;
-    std::vector<std::thread> threads;
-
-    size_t chunk_size = maxVal / threadsNumber;
-    for (size_t i = 0; i < threadsNumber; ++i) {
-        size_t start = i * chunk_size;
-        size_t end = (i == threadsNumber - 1) ? maxVal : (i + 1) * chunk_size;
-        threads.emplace_back(hack, data, "He-he-he", start, end, std::ref(badData));
-    }
-
-    for (auto& thread : threads) {
-        thread.join();
-    }
+  
+    hack(data, "He-he-he", badData);
 
     writeToFile(argv[2], badData);
 
